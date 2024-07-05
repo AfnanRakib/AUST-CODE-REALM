c5ccc6c30227aff3ccd42eac7d2a9546911d68cf
Rakib Hasan <afnanhasanrakib476@gmail.com>
2024-07-05 15:38:46 +0600

    -- navbar modified

--- helpers/navbar.php
+++ helpers/navbar.php
@@ -1,29 +1,33 @@
-    <?php $basePath = 'http://localhost/AUST%20CODE%20REALM/'; ?>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="<?php echo $basePath; ?>index.php"><img src="<?php echo $basePath; ?>images/logo.png" alt="ACR"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $basePath; ?>index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $basePath; ?>">Contests</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $basePath; ?>pages/problemPage.php">Problems</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $basePath; ?>">Discuss</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $basePath; ?>">Rankings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $basePath; ?>pages/profilePage.php">Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+<?php
+//session_start();
+$basePath = 'http://localhost:3000/';//'http://localhost/AUST%20CODE%20REALM/';
+$isLoggedIn = false;//isset($_SESSION['user_id']);
+?>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="<?php echo $basePath; ?>index.php"><img src="<?php echo $basePath; ?>images/logo.png" alt="ACR"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $basePath; ?>index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $basePath; ?>pages/contestSet.php">Contests</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $basePath; ?>pages/problemSet.php">Problems</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $basePath; ?>pages/courses.php">Courses</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $basePath; ?>pages/help.php">Help</a>
+            </li>
+        </ul>
+    </div>
+    <a class="profile-link" href="<?php echo $isLoggedIn ? $basePath . 'pages/profilePage.php' : $basePath . 'pages/login.php'; ?>">
+        <img src="<?php echo $basePath; ?>images/user.png" alt="Profile" class="profile-icon">
+    </a>
+</nav>
